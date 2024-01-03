4d45709c4641cb63829c885b9fc90209dd7a9a56
monakas <kkgtycb@yahoo.co.jp>
2024-01-04 02:39:27 +0900

    frontend側開発完了

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,10 @@ import router from './router'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHeart)
+
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,65 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '../views/WelcomeView'
+import ChatroomView from '@/views/ChatroomView.vue'
+import userValidate from '../auth/validate'
 // import HomeView from '../views/HomeView.vue'
+
+const { error, validate } = userValidate()
+
+// ESLintを無効にしたい時、コメント状態で下記を入れる
+// // es-lint-disable-next-line no-used-vars
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'WelcomeView'})
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'WelcomeView' })
+  } else {
+    next()
+  }
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'ChatroomView' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'WelcomeView',
-    component: WelcomeView
+    component: WelcomeView,
+    beforeEnter: noRequireAuth
+  },
+  {
+    path: '/chatroom',
+    name: 'ChatroomView',
+    component: ChatroomView,
+    beforeEnter: requireAuth
   }
 ]
 // const routes = [
